90e2636ed5f1fffafa11702bdc3cbd87cb7b1a3d
Kedjuprecious <kedjuprecious@gmail.com>
2023-06-21 16:43:15 +0100

    update code

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+  // DB: "mongodb+srv://kedjuprecious:5CluRIzHL3RUl3rw@reactbackend.smnwn9r.mongodb.net/ReactBackendProject?retryWrites=true&w=majority",
+  DB: "mongodb+srv://kedjuprecious:QuDZSCZ0MprgwmDN@react-backend.khmkpje.mongodb.net/ReactBackendProject?retryWrites=true&w=majority",
+};
+
+module.exports = {
+  MONGO_DB_CONFIG,
+};
